0c40ef5495183af6ad5353d0c99eaeeb5c239f09
Sindoori-14 <ksindoorinkl@gmail.com>
2026-04-16 14:33:52 +0530

    Migrate AppTest to JUnit 5 and enhance tests
    Refactor AppTest to use JUnit 5 and update test methods for library functionality.

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,19 +1,43 @@
 package com.example;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class AppTest {
+class AppTest {
 
     @Test
-    public void testAdd() {
-        App app = new App();
-        assertEquals(15, app.add(10,5));
+    void testAddBooks() {
+        App library = new App();
+        library.addBooks(10);
+        assertEquals(10, library.getAvailableBooks());
     }
 
     @Test
-    public void testSubtract() {
-        App app = new App();
-        assertEquals(5, app.subtract(10,5));
+    void testIssueBook() {
+        App library = new App();
+        library.addBooks(5);
+        library.issueBook();
+        assertEquals(4, library.getAvailableBooks());
+    }
+
+    @Test
+    void testReturnBook() {
+        App library = new App();
+        library.addBooks(5);
+        library.issueBook();
+        library.returnBook();
+        assertEquals(5, library.getAvailableBooks());
+    }
+
+    @Test
+    void testIssueBookWhenNoBooksAvailable() {
+        App library = new App();
+        assertThrows(IllegalArgumentException.class, library::issueBook);
+    }
+
+    @Test
+    void testInvalidAddBooks() {
+        App library = new App();
+        assertThrows(IllegalArgumentException.class, () -> library.addBooks(0));
     }
 }
